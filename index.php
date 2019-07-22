e782fa6e753249527ac4807bf5be9b5e35ed366c
超哥 <1659514328@qq.com>
2019-07-22 14:25:08 +0800

    发布

--- index.php
+++ index.php
@@ -5,10 +5,18 @@
 <title>哈哈</title>
 </head>
 <body>
-	<?php
-	
-	   $name = 10;
-	   echo $name;
-	?>
+	<nav class="navbar navbar-inverse navbar-fixed-top runoob-header" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+    			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+    				<span class="sr-only">菜单切换</span>
+    				<span class="icon-bar"></span>
+    				<span class="icon-bar"></span>
+    				<span class="icon-bar"></span>
+    			</button>
+    			<a class="navbar-brand runoob-title" href="https://c.runoob.com/">菜鸟工具</a>
+    		</div>
+		</div>
+	</nav>
 </body>
 </html>
